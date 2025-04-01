2a8d7612a67871bfb48c8478236f3dd1a4e201dc
ryuahneee <ryuahneee@gmail.com>
2025-04-01 23:12:01 +0900

    feat: detail.html 추가 및 Controller 매핑 추가
    - Controller RequestMapping /todos추가
    - detail.html 추가

--- src/main/java/com/demo/todoapps/web/TodoController.java
+++ src/main/java/com/demo/todoapps/web/TodoController.java
@@ -4,15 +4,13 @@ import com.demo.todoapps.core.application.TodoService;
 import com.demo.todoapps.core.domain.Todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/todos")
 public class TodoController {
     private final TodoService todoService;
 
